c6138007c5037ff5258cc99e78b84150cbc5b41e
niicooalv <n.alvarez2@liceolapaz.net>
2025-01-13 12:56:11 +0100

    Primer JS

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+function cambiarImagenFondo(ruta) {
+    const imagen = document.getElementById("miImagen");
+    imagen.src = ruta;
+}
+
+function cambiarTitulo(titulo) { // cambiar titulo y subtitulo podcast
+    const tituloElement = document.getElementById("pod_titulo");
+    tituloElement.innerHTML = titulo;
+
+}
+
+function cambiarDescripcion(descripcion) {
+    const descriptionElement = document.getElementById("pod_description");
+    descripcionElement.innerHTML = descripcion;
+}
+
+function seleccionarEpisodio(episodio) {
+   // cambiarImagenFondo(`img/${episodio}.jpg`); // se puede usar "img/${episodio}.jpg" siendo la variable episodio igual a episodio + el numero
+    cambiarImagenFondo(`img/episodio${episodio}.jpg`); // o usar identificar el episodio mediante un numero y usar "img/episodio${episodio}.jpg"
+    
+}
