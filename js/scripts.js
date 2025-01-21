ee7b8e5df69ab390cd0b0e9bd772b4803710fc02
niicooalv <n.alvarez2@liceolapaz.net>
2025-01-21 16:26:32 +0100

    Sistema de cambio de idioma
    Sistema de cambio de idioma y info de episodios

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,49 @@
+//
+const idiomaWeb = "es";
+document.documentElement.lang = idiomaInicial;
 
-function cambiarImagenFondo(ruta) {
-    const imagen = document.getElementById("miImagen");
-    imagen.src = ruta;
-}
+document.addEventListener('DOMContentLoaded', () => {
+    seleccionarEpisodio(1);
+
+});
+
+const episodios = [
+    {
+        id: 1,
+        es: {
+            titulo: "Episodio 1: Ayuda",
+            descripcion: "Ayuda sacadme de aqui."
+        },
+        en: {
+            titulo: "Help me please",
+            descripcion: "Help."
+        }
+    },
+    {
+        id: 2,
+        es: {
+            titulo: "Episodio 2: Descubriendo CSS",
+            descripcion: "CSS"
+        },
+        en: {
+            titulo: "Episode 2: Discovering CSS",
+            descripcion: "CSS"
+        }
+    },
+    {
+        id: 3,
+        es: {
+            titulo: "3.0 AYuda por favor",
+            descripcion: "Ayuda por favor"
+        },
+        en: {
+            titulo: "3.0 Help me please",
+            descripcion: "Help me please"
+        }
+    },
+
+];
+
 
 function cambiarTitulo(titulo) { // cambiar titulo del podcast
     const tituloElement = document.getElementById("pod_titulo");
@@ -15,8 +56,20 @@ function cambiarDescripcion(descripcion) { //cambiar descripción del podcast
     descripcionElement.innerHTML = descripcion;
 }
 
-function seleccionarEpisodio(episodio) {
-   // cambiarImagenFondo(`img/${episodio}.jpg`); // se puede usar "img/${episodio}.jpg" siendo la variable episodio igual a episodio + el numero
-    cambiarImagenFondo(`img/episodio${episodio}.jpg`); // o usar identificar el episodio mediante un numero y usar "img/episodio${episodio}.jpg"
-    
+function seleccionarEpisodio(episodioId) {
+    const episodio = episodios.find(e => e.id === episodioId);
+
+
+    // Cambiar imagen de fondo
+    //cambiarImagenFondo(`img/episodio${episodioId}.jpg`);
+
+    // Cambiar título y descripción según el idioma
+    if (idiomaWeb === "es") {
+        cambiarTitulo(episodio.es.titulo);
+        cambiarDescripcion(episodio.es.descripcion);
+    } else {
+        cambiarTitulo(episodio.en.titulo);
+        cambiarDescripcion(episodio.en.descripcion);
+    }
 }
+
